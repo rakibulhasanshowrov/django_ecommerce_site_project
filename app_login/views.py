616ee41ed1f1fc406cc3f3aec0caaa40b8ba4a0c
rakibulhasanshowrov <rakibulhasanshowrov@outlook.com>
2024-09-08 23:56:58 +0600

    0.3.5

--- app_login/views.py
+++ app_login/views.py
@@ -22,6 +22,7 @@ def sign_up(request):
     form=SignUpForm(request.POST)
     if form.is_valid():
       form.save()
+      messages.success(request, "Account Created Successfully!")
       return HttpResponseRedirect(reverse('app_login:login'))
   return render(request,'app_login/signup.html',context={'form':form})
 
@@ -44,3 +45,16 @@ def logout_user(request):
     logout(request)
     messages.warning(request, "You are logged out!!")
     return HttpResponse("logOut Successful")
+  
+@login_required
+def user_profile(request):
+  profile=Profile.objects.get(user=request.user)
+  form=ProfileForm(instance=profile)
+  if request.method=="POST":
+    form=ProfileForm(request.POST,instance=profile)
+    if form.is_valid():
+      form.save()
+      messages.success(request, "Change Saved!!")
+      form=ProfileForm(instance=profile)
+  return render(request,'app_login/change_profile.html',context={'form':form})
+  
